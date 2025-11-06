798046faa158983555e13a94eca4f5b2b0d742dd
monzzzz <abdallahmonzer2000@gmail.com>
2025-11-06 19:01:08 +0200

    Important part in programming

---
+++ Day01_Problems/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using DotSharp.Day01_problems;
+namespace Day01_problems;
+
+public class Program
+{
+	public static void Main()
+	{
+		Console.WriteLine("======Even Or Odd");
+		Problems.CheckEvenOdd();
+	}
+}
