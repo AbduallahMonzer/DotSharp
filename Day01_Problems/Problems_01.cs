e1206e276b2ebd6c6c7a6d00a294aa6203f599d3
monzzzz <abdallahmonzer2000@gmail.com>
2025-11-12 18:10:41 +0200

    simple console problems

--- Day01_Problems/Problems_01.cs
+++ Day01_Problems/Problems_01.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Runtime.ConstrainedExecution;
 namespace DotSharp.Day01_problems;
 
 class Problems
@@ -32,7 +33,32 @@ class Problems
 
 		if (num3 > max)
 			max = num3;
-		
+
 		Console.WriteLine($"The largest number is {max}");
 	}
+	
+	public static void GradeCalc()
+	{
+		Console.WriteLine("Enter your score: ");
+		if (!double.TryParse(Console.ReadLine(), out double score))
+		{
+			Console.WriteLine("Invalid input!");
+			return;
+		}
+		if (score < 0 || score > 100)
+		{
+			Console.WriteLine("The input should be (0-100)");
+			return;
+		}
+		string grade = score switch
+		{
+			>= 90 => "A",
+			>= 80 => "B",
+			>= 70 => "C",
+			>= 60 => "D",
+			_ => "F"
+		};
+		Console.WriteLine($"Grade is {grade}");
+	}
+		
 }
